e12aaec52e3f0e1640230e42a0a451efdeb5a447
farouk-root <farouk.chalghoumi031@gmail.com>
2024-05-08 08:35:12 +0200

    some path and routing fixes + fix tri Multi video

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -4,7 +4,7 @@ import {Router} from "@angular/router";
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent {
 
@@ -21,6 +21,6 @@ export class MenuComponent {
   goToCart() {
     this.router.navigate(['/cart']);
   }
-  
+
 
 }
